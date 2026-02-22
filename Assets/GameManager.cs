1d7724137c1f7678b83dc89da252961d6e8997a1
cat-sun-1234 <chentongsun1320@gmail.com>
2026-02-22 23:03:06 +1100

    basic movement and asset edits.

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,16 +1,25 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public int SoldiersInHelicopter = 0;
+    public int SoldiersRescued = 0;
+    public int SoldiersOnField;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        //if (Input.GetKeyDown(KeyCode.R))
+       // {
+           // RestartGame();
+        //}
     }
 }
---
+++ Assets/HelicopterController.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class HelicopterController : MonoBehaviour
+{
+
+    public float moveSpeed = 5f;
+    [SerializeField]
+    private Rigidbody2D rb2d;
+
+    [SerializeField]
+    private Vector2 moveDirection;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+            // Get horizontal and vertical input (from arrow keys or WASD)
+            float moveHorizontal = Input.GetAxis("Horizontal"); //
+            float moveVertical = Input.GetAxis("Vertical");     //
+
+            // Calculate movement vector
+            moveDirection = new Vector2(moveHorizontal, moveVertical);
+    }
+
+    void FixedUpdate()
+    {
+    if (rb2d != null)
+    {
+        rb2d.linearVelocity = moveDirection * moveSpeed;
+    }
+    }
+}
